bc0294df174738a7180d8617fb0f0e64a063506d
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-11-26 12:09:08 -0300

    filter 4th step

--- work_methods.py
+++ work_methods.py
@@ -96,3 +96,70 @@ def info_gain_matrix_recover(info_gain_database:pd.DataFrame, square_reference:i
                         line_breaker = 0
     
     return info_gain_matrix
+
+def create_filter_array(square_reference:int, start_rows:int, start_columns:int, last_rows:int, last_columns:int) -> list:
+    filtr = np.zeros((28,28))
+    for i in range(start_rows, last_rows):
+        for j in range(start_columns, last_columns):
+            filtr[i][j] = 1
+
+    filtr_array = []
+    for i in range(28):
+        for j in range(28):
+            filtr_array.append(int(filtr[i][j]))
+            
+    # key 2 to indicate the class
+    filtr_array.append(2)
+    
+    return filtr_array
+
+def create_selected_base(input_file_name:str, output_file_name:str, filtr_array:list) -> None:
+    #handling file and filter by info gain 
+    new_file = open(output_file_name, 'w')
+    with open(input_file_name, 'r') as file:
+        first_row = True
+        for line in file:
+            row_to_write = ''
+            items = line.split(',')
+            for i in range(len(items)):
+                if filtr_array[i] == 1:
+                    row_to_write += items[i]+','
+                elif filtr_array[i] == 2:
+                    row_to_write += items[i]
+            nan = False
+            if not first_row:
+                for item in row_to_write:
+                    if not item.isnumeric() and not item.isalpha() and item!=',' and item!='\n':
+                        nan = True
+            if row_to_write.count(',') == 400 and not nan:
+                new_file.write(row_to_write)
+            first_row = False
+
+    new_file.close()
+
+def create_binary_base(input_file_name:str, output_file_name:str) -> None:
+    #handling file and transform numbers to 0 or 1
+    new_file = open(output_file_name, 'w')
+    with open(input_file_name, 'r') as file:
+        first_row = True
+        for line in file:
+            row_to_write = ''
+            items = line.split(',')
+            for i in range(len(items)):
+                if first_row:
+                    if '\n' in items[i]:
+                        row_to_write += items[i]
+                    else:
+                        row_to_write += items[i]+','
+                else:
+                    if '\n' in items[i]:
+                        row_to_write += items[i]
+                    else:
+                        if float(items[i]) == 0.0:
+                            row_to_write += '0,'
+                        else:
+                            row_to_write += '1,'
+            new_file.write(row_to_write)
+            first_row = False
+
+    new_file.close()
