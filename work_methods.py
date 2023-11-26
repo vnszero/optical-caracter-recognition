5cec99c0fbb42684ffdf947e1d4f12c42fe0d24c
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-11-26 00:08:06 -0300

    2nd step, crude data

--- work_methods.py
+++ work_methods.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import hiplot as hip
+from avaliacao import Experimento
 
 def matrix_recover(all_data:pd.DataFrame, to_show:int, square_reference:int) -> list:
     line_breaker = 0
@@ -37,3 +39,35 @@ def matrix_recover(all_data:pd.DataFrame, to_show:int, square_reference:int) -> 
             break
 
     return matrix
+
+def show_results(name:str, numbers:dict, exp:Experimento) -> None:
+    print(name)
+    for resultado in exp.resultados:
+        print("Macro f1: " + str(resultado.macro_f1))
+        print("\nAcuracia: " + str(resultado.acuracia))
+        print("\nf1 por classe: ")
+        for key in resultado.f1_por_classe.keys():
+            print('\t{} -> {}'.format(numbers[key], resultado.f1_por_classe[key]))
+        print("\nprecisao: ")
+        for key in resultado.precisao.keys():
+            print('\t{} -> {}'.format(numbers[key], resultado.precisao[key]))
+        print("\nrevocacao: ")
+        for key in resultado.revocacao.keys():
+            print('\t{} -> {}'.format(numbers[key], resultado.revocacao[key]))
+        print('\nMatriz de confusao: ')
+        print("\t", end='')
+        for names in numbers.values():
+            print(names[:2]+"\t", end='')
+        print()
+        for i in resultado.mat_confusao:
+            print(numbers[i][0:2]+"\t", end='')
+            for j in resultado.mat_confusao[i]:
+                print(str(resultado.mat_confusao[i][j])+"\t", end='')
+            print()
+        print('\n')
+        print('===================================================================\n')
+    
+def parameters_graph(trials_fold) -> None:
+    # build a graph for passed trials fold parameters swap
+    data = [{**trial.params, 'loss': trial.value} for trial in trials_fold]
+    hip.Experiment.from_iterable(data).display(force_full_width=True)
