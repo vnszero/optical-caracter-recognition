3a8f7ff6d5186224c4ae0f5beaa1392b2e7cb259
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-11-26 21:00:50 -0300

    method code review

---
+++ method.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from results import Results
+import pandas as pd
+from sklearn.base import ClassifierMixin, RegressorMixin
+from typing import List,Union
+
+class MachineLearningMethod:
+
+    @abstractmethod
+    def eval(self,df_practice:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_category:str) -> Results:
+        raise NotImplementedError
+
+class ScikitLearnMachineLearning(MachineLearningMethod):
+    # ml_method is a ClassifierMixin or RegressorMixin
+    # both are superclasses
+    def __init__(self,ml_method:Union[ClassifierMixin,RegressorMixin]):
+        self.ml_method = ml_method
+
+    def eval(self, df_practice:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_category:str, seed:int=1) -> Results:
+        # from df_practice, split features from category
+        # x_practice holds features
+        # y_practice holds category value
+        x_practice = df_practice.drop(col_category,axis=1)
+        y_practice = df_practice[col_category]
+
+        # execute the fit method of ml_method e create the model
+        model = self.ml_method.fit(x_practice,y_practice)
+        # split to predict data in x and y groups
+        x_to_predict = df_data_to_predict.drop(col_category,axis=1)
+        y_to_predict = df_data_to_predict[col_category]
+
+        # return results
+        y_predictions = model.predict(x_to_predict)
+        return Results(y_to_predict,y_predictions)
